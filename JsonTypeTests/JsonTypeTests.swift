f37980483efde5160645ea43190ff8dd121ba20a
notbakaneko <notbakaneko@users.noreply.github.com>
2015-02-09 13:19:23 +0900

    Add tests

--- JsonTypeTests/JsonTypeTests.swift
+++ JsonTypeTests/JsonTypeTests.swift
@@ -6,11 +6,26 @@
 //  Copyright (c) 2015 nekonyan. All rights reserved.
 //
 
-import UIKit
+import Foundation
 import XCTest
+import JsonType
+
 
 class JsonTypeTests: XCTestCase {
-    
+    lazy var testDictionary: NSDictionary = {
+        return [
+            "string": "This is a string.",
+            "scalar": 3,
+            "scalar_string": "7",
+            "float": 0.9,
+            "float_string": "0.9",
+            "bool": true,
+            "bool_string": "true",
+            "array": [ "a", "b", "c", "d" ],
+            "dictionary": [ "k": "v" ]
+        ]
+    }()
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -20,17 +35,244 @@ class JsonTypeTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+
+
+    func test_String() {
+        let iv = ""
+        var string = iv
+        string <- testDictionary["string"]
+        XCTAssert(string == "This is a string.")
+
+        var scalar = iv
+        scalar <- testDictionary["scalar"]
+        XCTAssert(scalar == iv)
+
+        var scalarString = iv
+        scalarString <- testDictionary["scalar_string"]
+        XCTAssert(scalarString == "7")
+
+        var float = iv
+        float <- testDictionary["float"]
+        XCTAssert(float == iv)
+
+        var floatString = iv
+        floatString <- testDictionary["float_string"]
+        XCTAssert(floatString == "0.9")
+
+        var bool = iv
+        bool <- testDictionary["bool"]
+        XCTAssert(bool == iv)
+
+        var boolString = iv
+        boolString <- testDictionary["bool_string"]
+        XCTAssert(boolString == "true")
+
+        var array = iv
+        array <- testDictionary["array"]
+        XCTAssert(array == iv)
+
+        var dictionary = iv
+        dictionary <- testDictionary["dictionary"]
+        XCTAssert(dictionary == iv)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
+
+
+    func test_Int() {
+        let iv = 0
+        var string = iv
+        string <- testDictionary["string"]
+        XCTAssert(string == iv)
+
+        var scalar = iv
+        scalar <- testDictionary["scalar"]
+        XCTAssert(scalar == 3)
+
+        var scalarString = 0
+        scalarString <- testDictionary["scalar_string"]
+        XCTAssert(scalarString == 7)
+
+        var float = iv
+        float <- testDictionary["float"]
+        XCTAssert(float == iv)
+
+        var floatString = iv
+        floatString <- testDictionary["float_string"]
+        XCTAssert(floatString == iv)
+
+        var bool = iv
+        bool <- testDictionary["bool"]
+        XCTAssert(bool == 1)
+
+        var boolString = iv
+        boolString <- testDictionary["bool_string"]
+        XCTAssert(boolString == iv)
+
+        var array = iv
+        array <- testDictionary["array"]
+        XCTAssert(array == iv)
+
+        var dictionary = iv
+        dictionary <- testDictionary["dictionary"]
+        XCTAssert(dictionary == iv)
     }
-    
+
+
+    func test_Double() {
+        let iv = 0.0
+        var string = iv
+        string <- testDictionary["string"]
+        XCTAssert(string == iv)
+
+        var scalar = iv
+        scalar <- testDictionary["scalar"]
+        XCTAssert(scalar == 3.0)
+
+        var scalarString = iv
+        scalarString <- testDictionary["scalar_string"]
+        XCTAssert(scalarString == 7.0)
+
+        var float = iv
+        float <- testDictionary["float"]
+        XCTAssert(float == 0.9)
+
+        var floatString = iv
+        floatString <- testDictionary["float_string"]
+        XCTAssert(floatString == 0.9)
+
+        var bool = iv
+        bool <- testDictionary["bool"]
+        XCTAssert(bool == 1)
+
+        var boolString = iv
+        boolString <- testDictionary["bool_string"]
+        XCTAssert(boolString == iv)
+
+        var array = iv
+        array <- testDictionary["array"]
+        XCTAssert(array == iv)
+
+        var dictionary = iv
+        dictionary <- testDictionary["dictionary"]
+        XCTAssert(dictionary == iv)
+    }
+
+
+    func test_Bool() {
+        let iv = false
+        var string = iv
+        string <- testDictionary["string"]
+        XCTAssert(string == iv)
+
+        var scalar = iv
+        scalar <- testDictionary["scalar"]
+        XCTAssert(scalar == true)
+
+        var scalarString = iv
+        scalarString <- testDictionary["scalar_string"]
+        XCTAssert(scalarString == iv)
+
+        var float = iv
+        float <- testDictionary["float"]
+        XCTAssert(float == true)
+
+        var floatString = iv
+        floatString <- testDictionary["float_string"]
+        XCTAssert(floatString == iv)
+
+        var bool = iv
+        bool <- testDictionary["bool"]
+        XCTAssert(bool == true)
+
+        var boolString = iv
+        boolString <- testDictionary["bool_string"]
+        XCTAssert(boolString == iv)
+
+        var array = iv
+        array <- testDictionary["array"]
+        XCTAssert(array == iv)
+
+        var dictionary = iv
+        dictionary <- testDictionary["dictionary"]
+        XCTAssert(dictionary == iv)
+    }
+
+
+    func test_Array() {
+        let iv = NSArray()
+        var string = iv
+        string <- testDictionary["string"]
+        XCTAssert(string == iv)
+
+        var scalar = iv
+        scalar <- testDictionary["scalar"]
+        XCTAssert(scalar == iv)
+
+        var scalarString = iv
+        scalarString <- testDictionary["scalar_string"]
+        XCTAssert(scalarString == iv)
+
+        var float = iv
+        float <- testDictionary["float"]
+        XCTAssert(float == iv)
+
+        var floatString = iv
+        floatString <- testDictionary["float_string"]
+        XCTAssert(floatString == iv)
+
+        var bool = iv
+        bool <- testDictionary["bool"]
+        XCTAssert(bool == iv)
+
+        var boolString = iv
+        boolString <- testDictionary["bool_string"]
+        XCTAssert(boolString == iv)
+
+        var array = iv
+        array <- testDictionary["array"]
+        XCTAssert(array == [ "a", "b", "c", "d" ])
+
+        var dictionary = iv
+        dictionary <- testDictionary["dictionary"]
+        XCTAssert(dictionary == iv)
+    }
+
+
+    func test_Dictionary() {
+        let iv = NSDictionary()
+        var string = iv
+        string <- testDictionary["string"]
+        XCTAssert(string == iv)
+
+        var scalar = iv
+        scalar <- testDictionary["scalar"]
+        XCTAssert(scalar == iv)
+
+        var scalarString = iv
+        scalarString <- testDictionary["scalar_string"]
+        XCTAssert(scalarString == iv)
+
+        var float = iv
+        float <- testDictionary["float"]
+        XCTAssert(float == iv)
+
+        var floatString = iv
+        floatString <- testDictionary["float_string"]
+        XCTAssert(floatString == iv)
+
+        var bool = iv
+        bool <- testDictionary["bool"]
+        XCTAssert(bool == iv)
+
+        var boolString = iv
+        boolString <- testDictionary["bool_string"]
+        XCTAssert(boolString == iv)
+
+        var array = iv
+        array <- testDictionary["array"]
+        XCTAssert(array == iv)
+
+        var dictionary = iv
+        dictionary <- testDictionary["dictionary"]
+        XCTAssert(dictionary == ["k": "v"])
+    }
 }
